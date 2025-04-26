f2d5d90b383dc25f73c1e364c2d931ff4a5c4357
Andrey <medvedevandrey860@gmail.com>
2025-04-26 14:25:49 +0300

    modified:   main.go SecondCommit

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	var name string
+	var age int
+
+	var message string = "Hellow"
+	var count int = 42
+
+	var gretting = "Good Morning"
+	var num = 100
+
+	var (
+		fName string = "A"
+		lName string = "B"
+		year  int    = 52
+	)
+
 	fmt.Print("hello world")
+	fmt.Print(name)
 }
