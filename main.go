4a8c760e1be62cc5fe8b18ca2a798fc9e4797cd0
Andrey <medvedevandrey860@gmail.com>
2025-05-03 13:33:28 +0300

    n1

--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
 func main() {
-	a := 20
-	b := 11
-
-	sum := a + b
-	fmt.Printf("%d + %d = %d\n", a, b, sum)
-
-	dif := a - b
-	fmt.Printf("%d - %d = %d\n", a, b, dif)
-
-	prod := a * b
-	fmt.Printf("%d * %d = %d\n", a, b, prod)
-
-	quo := a / b
-	fmt.Printf("%d / %d = %d\n", a, b, quo)
-
-	rem := a % b
-	fmt.Printf("%d %% %d = %d\n", a, b, rem)
-
-	FloatQ := float64(a) / float64(b)
-	fmt.Printf("%d / %d = %.3f\n", a, b, FloatQ)
-
-	fmt.Print(math.Pow(float64(a), 2))
+	age := 16
+	isAdult := age >= 18
+	hasPerm := isLoggedIn && hasRole
+	canAccess := isAdmin || hasPerm
+	isRestricted := !canAccess
 }
