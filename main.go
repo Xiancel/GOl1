43dc3fd77adbb48af3d19d61ef12a830b29b6bd4
Andrey <medvedevandrey860@gmail.com>
2025-05-03 13:54:01 +0300

    ifElses

--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	password := "i123nqwe"
-	if len(password) >= 8 {
-		fmt.Println("Password has successful leng")
+	currentHour := time.Now().Hour()
+	fmt.Println(currentHour)
+
+	if currentHour >= 10 && currentHour < 17 {
+		fmt.Println("зараз день")
 	} else {
-		fmt.Println("Password no has successful leng")
+		fmt.Println("зараз ніч")
 	}
 
+	if rand.Intn(2) == 0 {
+		fmt.Println("Орел")
+	} else {
+		fmt.Println("Решка")
+	}
+
+	satusCode := 200
+	if satusCode >= 200 && satusCode < 300 {
+		fmt.Println("OK")
+	} else {
+		fmt.Println("Nook")
+	}
 }
