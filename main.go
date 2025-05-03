8e7c97dd01480b7f59cb8c3d17404f2b34353874
Andrey <medvedevandrey860@gmail.com>
2025-05-03 13:38:56 +0300

    ifElse

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	age := 16
-	isAdult := age >= 18
-	hasPerm := isLoggedIn && hasRole
-	canAccess := isAdmin || hasPerm
-	isRestricted := !canAccess
+	password := "i123nqwe"
+	if len(password) >= 8 {
+		fmt.Println("Password has successful leng")
+	} else {
+		fmt.Println("Password no has successful leng")
+	}
+
 }
