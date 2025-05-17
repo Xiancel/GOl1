93e1e52b07cdf586f22403b2b57edc4e4dff8293
Andrey <medvedevandrey860@gmail.com>
2025-05-17 14:07:38 +0300

    func lvl2

--- main.go
+++ main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func output(a int) {
-	fmt.Println(a)
-}
 func main() {
-	var a int
+	fmt.Println(divide(10, 2))
 
-	fmt.Scanln(&a)
+	q, r := divideWithRemainder(13, 5)
+	fmt.Printf("13 / 5 = %d with remainder %d\n", q, r)
 
-	output(a)
+	number, _, _ := getDate()
+	fmt.Println("Number: ", number)
+
+	_, _, isValid := getDate()
+	fmt.Println("Correct: ", isValid)
+
+	res, err := divideErr(10, 2)
+	if err != nil {
+		fmt.Println("Errors: ", err)
+	} else {
+		fmt.Println("Result: ", res)
+	}
+
+	res, err = divideErr(10, 0)
+	if err != nil {
+		fmt.Println("Errors: ", err)
+	} else {
+		fmt.Println("Result: ", res)
+	}
 }
 
-// func functionName (par1 type1, par2 type2, ...)return_type{
-// 	//function body
-// 	return value
-// }
+func divide(a, b float64) (res float64) {
+	res = a / b
+	return
+}
+
+func divideWithRemainder(a, b int) (int, int) {
+	q := a / b
+	r := a % b
+	return q, r
+}
+
+func getDate() (int, string, bool) {
+	return 42, "hellow", true
+}
+
+func divideErr(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("divide by zero")
+	}
+	return a / b, nil
+}
