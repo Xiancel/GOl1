6cd5e6cdead4c115cee484a4a5537288ff4e473e
Andrey <medvedevandrey860@gmail.com>
2025-05-24 14:26:39 +0300

    collection slice remover v2

--- main.go
+++ main.go
@@ -16,8 +16,17 @@ func main() {
 	fmt.Println(" --- Після видалення --- ")
 	names = removeElement(names, 2)
 	fmt.Println(names)
+
+	names = removeElement(names, 2)
+	fmt.Println(names)
 }
 
 func removeElement(slice []string, index int) []string {
 	return append(slice[:index], slice[index+1:]...)
+}
+
+func removeFastElement(slice []string, index int) []string {
+	slice[index] = slice[len(slice)-1]
+
+	return slice[:len(slice)-1]
 }
