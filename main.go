5935c4a57d1619389b6d0f4269ef305d32447a42
Andrey <medvedevandrey860@gmail.com>
2025-05-31 20:33:43 +0300

    value vs raference type BEFORE

--- main.go
+++ main.go
@@ -2,46 +2,18 @@ package main
 
 import "fmt"
 
-type Car struct {
-	Brand      string
-	Model      string
-	Years      int
-	Price      float64
-	IsElectric bool
-}
-
-type Garage struct {
-	Cars []Car
+type Studen struct {
+	Name    string
+	Age     int
+	Group   string
+	Avarage float64
 }
 
 func main() {
-	car1 := Car{
-		Brand:      "BMW",
-		Model:      "M4",
-		Years:      2024,
-		Price:      80000,
-		IsElectric: false,
-	}
-	car2 := Car{
-		Brand:      "BMW",
-		Model:      "X6M",
-		Years:      2025,
-		Price:      132100,
-		IsElectric: false,
-	}
-	car3 := Car{
-		Brand:      "Porsche",
-		Model:      "Taycan turbo s",
-		Years:      2023,
-		Price:      168280,
-		IsElectric: true,
-	}
+	a := Studen{Name: "Shanks", Age: 39}
+	b := a
+	b.Name = "Bob"
 
-	garages := Garage{
-		Cars: []Car{car1, car2, car3},
-	}
-
-	for i := 0; i < len(garages.Cars); i++ {
-		fmt.Printf("Cars %d: %+v\n", i+1, garages.Cars[i])
-	}
+	fmt.Println(a.Name)
+	fmt.Println(b.Name)
 }
