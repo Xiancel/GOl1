5ac61d06146f618df0690039ba1b19c24cf16a7f
Andrey <medvedevandrey860@gmail.com>
2025-05-31 19:18:31 +0300

    custome type

--- main.go
+++ main.go
@@ -4,32 +4,15 @@ import (
 	"fmt"
 )
 
+// custome type
+type names string
+type km float64
+
 func main() {
-	//мапа - це невпорядкована колекція пар ключ-значень, де ключі УНІКАЛЬНІ
-	//var name map[KeyType]ValueType
-	//var scores map[string]int
-
-	userAges := map[string]int{
-		"Oleg":      52,
-		"Oleksandr": 49,
-		"Jesse":     125,
-	}
-	fmt.Println(userAges)
-
-	contacts := make(map[string]string)
-
-	contacts["Oleksandr"] = "+3800505050123"
-	contacts["Oleg"] = "+380123456789010"
-
-	fmt.Println(contacts)
-
-	phone, exists := contacts["Oleg"]
-	if exists {
-		fmt.Println(phone)
-	} else {
-		fmt.Println("nothing")
-	}
-
-	delete(contacts, "Oleg")
-	fmt.Println(contacts)
+	var jrl names = "Joseph"
+	var jr2 names = "Dio"
+	fmt.Println(jrl)
+	fmt.Println(jr2)
+	var section km = 64.5
+	fmt.Println(section)
 }
