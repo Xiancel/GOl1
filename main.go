4ae2def701d4c0802eded91dc549470db8db6985
Andrey <medvedevandrey860@gmail.com>
2025-06-07 14:56:05 +0300

    interface. 3

--- main.go
+++ main.go
@@ -2,50 +2,16 @@ package main
 
 import "fmt"
 
-// Інтерфейси - це колекція сигнатур методів. Це контракт, який визначає, які методи повинен мати тип, але НЕ РЕАЛІЗУЄ ЇХ
-//  type НазваІнтерфейсу interface{
-// 	НазваМетоду(параметр) повертає
-// 	ІншийМетод(параметр) повертає
-//  }
-
-//Duck type - "якщо щось ходить як качка і крякає як качка, то це качка!!!"
-type Drawble interface {
-	Draw()
-}
-
-type Resizeble interface {
-	Resizeble(factor float64)
-}
-
-type GraphicElement interface {
-	Drawble
-	Resizeble
-	GetInfo() string
-}
-
-type Square struct {
-	Size float64
-}
-
-func (s *Square) Draw() {
-	fmt.Printf("Draw square with size: %.f \n", s.Size)
-}
-
-func (s *Square) Resizeble(factor float64) {
-	s.Size *= factor
-	fmt.Printf("Draw square with size: %.f \n", s.Size)
-}
-func (s Square) GetInfo() string {
-	return fmt.Sprintf("Square: sise=%.1f\n", s.Size)
-}
-
-func ProcessGraphic(g GraphicElement) {
-	fmt.Println(g.GetInfo())
-	g.Draw()
-	g.Resizeble(1.5)
-	g.Draw()
-}
 func main() {
-	sq := &Square{Size: 10}
-	ProcessGraphic(sq)
+	PrintAnything(42)
+	PrintAnything(52.2)
+	PrintAnything("Welcome to the club body")
+	PrintAnything(true)
+	PrintAnything([]int{1, 52, 78, 9})
+	PrintAnything('H')
+
+}
+
+func PrintAnything(value interface{}) {
+	fmt.Printf("Value: %v, Type: %T \n", value, value)
 }
